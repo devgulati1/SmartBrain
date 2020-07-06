06de7c8b98a472f8b27a7d0a3a5ef2f88ad56c35
devgulati1 <gulatidev84@gmail.com>
2020-07-06 19:57:53 +0530

    Updates

---
+++ precache-manifest.b29a12003b882596231a8813708a5240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c02ab6f82bfda3e6155e2af251abc51",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "b96714de551d4577615f",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "72007ac617cbb5f32b4a",
+    "url": "/SmartBrain/static/css/main.a0e95557.chunk.css"
+  },
+  {
+    "revision": "b96714de551d4577615f",
+    "url": "/SmartBrain/static/js/2.0dd3dd91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SmartBrain/static/js/2.0dd3dd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72007ac617cbb5f32b4a",
+    "url": "/SmartBrain/static/js/main.655e7e32.chunk.js"
+  },
+  {
+    "revision": "ae58e2945256ea8a5302",
+    "url": "/SmartBrain/static/js/runtime-main.74890f84.js"
+  }
+]);
